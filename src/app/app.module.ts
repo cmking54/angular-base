25c1232a22930f2253ea6929e9bd8370cec3d74b
cmking54 <cmking5496@gmail.com>
2019-10-04 11:29:45 -0400

    Initial Commit; everything linked up and displaying when served

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; // ngmodule marks modules
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; // no ext
 
-@NgModule({
-  declarations: [
+@NgModule({ // not a statement; but a descriptor
+  declarations: [ // bring in components, directives, and pipes not from another module
     AppComponent
   ],
-  imports: [
+  imports: [ // bring in other modules that are needed
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // mark the starting point for the bootstrap process
 })
 export class AppModule { }
